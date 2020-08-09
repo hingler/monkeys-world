7c865604c68a7b32ff2a20fc5e2aef3011b0c779
jamieboy1337 <jamieboy1337@gmail.com>
2020-08-08 17:29:45 -0700

    created layer group to contain layers

---
+++ include/layer/LayerGroup.hpp
@@ -0,0 +1,29 @@
+#ifndef LAYER_GROUP_H_
+#define LAYER_GROUP_H_
+
+#include "layer/Layer.hpp"
+#include <memory>
+
+/**
+ *  Layer groups contain several layers
+ */ 
+class LayerGroup : Layer {
+ public:
+  /**
+   *  Add a layer to this layer group. This group assumes ownership of the layer.
+   */ 
+  void AddLayer(std::shared_ptr<Layer> layer);
+
+  /**
+   *  Removes a child layer based on a pointer to another layer.
+   */ 
+  void RemoveChild(std::shared_ptr<Layer> layer);
+
+
+  /**
+   *  Removes a child layer based on a layer's ID which appears somewhere in the hierarchy.
+   */ 
+  void RemoveChild(std::string layerId);
+};
+
+#endif
