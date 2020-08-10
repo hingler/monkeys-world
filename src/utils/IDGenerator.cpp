2e2724afbfbd458df4f32d22f853e0260f9d8f54
jamieboy1337 <jamieboy1337@gmail.com>
2020-08-09 23:00:50 -0700

    Created IDGenerator for making unique IDs

---
+++ src/utils/IDGenerator.cpp
@@ -0,0 +1,22 @@
+#include "utils/IDGenerator.hpp"
+
+IDGenerator::IDGenerator() : counter_(0) { };
+
+uint64_t IDGenerator::CreateUniqueID() {
+  std::lock_guard(id_lock_);
+  while (identifiers_.find(counter_) != identifiers_.end()) {
+    ++counter_;
+  }
+
+  identifiers_.insert(counter_);
+  return counter_++;
+}
+
+void IDGenerator::InsertUniqueID(uint64_t id) {
+  identifiers_.insert(id);
+}
+
+bool IDGenerator::IsIDUnique(uint64_t targetID) {
+  std::lock_guard(id_lock_);
+  return (identifiers_.find(targetID) != identifiers_.end());
+}
