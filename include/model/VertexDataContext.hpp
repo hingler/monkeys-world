a42e8670fb7acbc3ed8b78cf7d41102bd2afe536
jamieboy1337 <jamieboy1337@gmail.com>
2020-09-20 02:16:09 -0700

    updated comment on vertexdatacontext

--- include/model/VertexDataContext.hpp
+++ include/model/VertexDataContext.hpp
@@ -12,7 +12,9 @@ class VertexDataContext {
  public:
 
   /**
-   *  
+   *  Populates the GL buffers managed by this context, then points to their appropriate attributes.
+   *  @param data - The vertex attribute data.
+   *  @param indices - triplets of indices representing triangles.
    */ 
   virtual void PopulateBuffersAndPoint(const std::vector<Packet>& data, const std::vector<int>& indices) = 0;
 };
