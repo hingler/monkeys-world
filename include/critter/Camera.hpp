5054c63e4b5c530df672f6cedc0dd7a7b03c3a77
jamieboy1337 <jamieboy1337@gmail.com>
2020-10-08 21:48:14 -0700

    created camera header

---
+++ include/critter/Camera.hpp
@@ -0,0 +1,19 @@
+#ifndef CAMERA_H_
+#define CAMERA_H_
+
+namespace screenspacemanager {
+namespace critter {
+  /**
+   *  Represents the camera within a scene.
+   */ 
+class Camera {
+ public:
+  /**
+   *  Constructs a new camera.
+   */ 
+  Camera(); 
+};
+}
+}
+
+#endif
