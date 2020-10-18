72b570a5d2a315c926621143186e25329dabf28f
jamieboy1337 <jamieboy1337@gmail.com>
2020-10-18 16:28:03 -0700

    fixed some weird bind logic

--- src/storage/VertexPacketTypes.cpp
+++ src/storage/VertexPacketTypes.cpp
@@ -1,5 +1,6 @@
 #include "storage/VertexPacketTypes.hpp"
 
+#include <boost/log/trivial.hpp>
 #include <glm/glm.hpp>
 #include <glad/glad.h>
 
@@ -18,16 +19,19 @@ void VertexPacket2D::Bind() {
 
 void VertexPacket3D::Bind() {
   // position data (position 0)
-  glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(VertexPacket3D), (void*)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(VertexPacket3D), (void*)0);
   glEnableVertexAttribArray(0);
+  
 
   // tex data (position 1)
-  glVertexAttribPointer(1, 2, GL_FLOAT, false, sizeof(VertexPacket3D), (void*)(6 * sizeof(float)));
-  glEnableVertexAttribArray(2);
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(VertexPacket3D), (void*)(3 * sizeof(float)));
+  
 
   // normal data (position 2)
-  glVertexAttribPointer(2, 3, GL_FLOAT, false, sizeof(VertexPacket3D), (void*)(3 * sizeof(float)));
-  glEnableVertexAttribArray(1);
+  glEnableVertexAttribArray(2);
+  glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(VertexPacket3D), (void*)(5 * sizeof(float)));
+  
 }
 
 };  // namespace storage
