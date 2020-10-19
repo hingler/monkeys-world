64381467e81d71d7f8e4b980ec1d90d21d8529f7
jamieboy1337 <jamieboy1337@gmail.com>
2020-10-19 13:14:18 -0700

    some basic shit for object class

---
+++ include/critter/Object.hpp
@@ -0,0 +1,19 @@
+#ifndef OBJECT_H_
+#define OBJECT_H_
+
+namespace screenspacemanager {
+namespace critter {
+
+/**
+ *  The Object class represents a unit of execution within the game engine.
+ *  Objects must be able to draw themselves onto the screen, as well as
+ *  respond to in-game events broadcast elsewhere in the engine.
+ */ 
+class Object {
+
+}
+
+} // namespace critter
+} // namespace screenspacemanager
+
+#endif  // OBJECT_H_
