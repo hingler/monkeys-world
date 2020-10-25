c0359b73cead843b21fbdc8f036617c239bea518
jamieboy1337 <jamieboy1337@gmail.com>
2020-10-25 13:15:52 -0700

    created test for verifying cache contents

--- test/CacheFileLoaderTest.cpp
+++ test/CacheFileLoaderTest.cpp
@@ -2,6 +2,7 @@
 
 #include <gtest/gtest.h>
 
+#include <fstream>
 #include <iostream>
 #include <thread>
 
@@ -16,6 +17,8 @@ TEST(CacheTests, CreateEmptyCache) {
 }
 
 TEST(CacheTests, ReadExistingCache) {
+  remove("resources/cache/existingcache.filecache");
+
   {
     CachedFileLoader loader("existingcache");
     loader.LoadFile("resources/glsl/matte-material/matte-material.vert");
@@ -24,4 +27,23 @@ TEST(CacheTests, ReadExistingCache) {
   }
 
   CachedFileLoader loader("existingcache");
+}
+
+// verify contents (should be fine lol)
+TEST(CacheTests, ReadCacheContents) {
+  remove("resources/cache/coolcache.filecache");
+  CachedFileLoader loader("coolcache");
+  std::unique_ptr<std::streambuf> test = loader.LoadFile("resources/themoney.txt");
+  std::unique_ptr<std::streambuf> testtwo = loader.LoadFile("resources/glsl/matte-material/matte-material.frag");
+  std::istream test_cache(test.get());
+  std::istream testtwo_cache(testtwo.get());
+  std::ifstream test_verify("resources/themoney.txt", std::ifstream::in | std::ifstream::binary);
+  std::ifstream testtwo_verify("resources/glsl/matte-material/matte-material.frag", std::ifstream::in | std::ifstream::binary);
+  while (!test_cache.eof()) {
+    ASSERT_EQ(test_verify.get(), test_cache.get());
+  }
+  uint32_t i = 0;
+  while (!testtwo_cache.eof()) {
+    ASSERT_EQ(testtwo_verify.get(), testtwo_cache.get());
+  }
 }
