2ca8c7b65713c1bb3814a20f164e64bd4d3975d7
jamieboy1337 <jamieboy1337@gmail.com>
2020-10-24 17:41:55 -0700

    added methods for direct byte r/w

--- include/utils/FileUtils.hpp
+++ include/utils/FileUtils.hpp
@@ -19,6 +19,29 @@ namespace fileutils {
  */ 
 uint32_t CalculateCRCHash(std::istream& input, std::streamoff offset);
 
+/**
+ *  Writes to a file as bytes.
+ *  @param <input_type>: The type being written.
+ *  @param output - the output stream being written to.
+ *  @param data - the data being written.
+ */ 
+template <typename input_type>
+void WriteAsBytes(std::ostream& output, input_type data) {
+  output.write(reinterpret_cast<const char*>(&data), sizeof(input_type));
+}
+
+/**
+ *  Reads bytes from a file to a return object.
+ *  @param <output_type>: The type being read.
+ *  @param input - the stream being read from.
+ */ 
+template <typename output_type>
+output_type ReadAsBytes(std::istream& input) {
+  output_type output;
+  input.read(reinterpret_cast<char*>(&output), sizeof(output_type));
+  return output;
+}
+
 } // namespace fileutils
 } // namespace utils
 } // namespace screenspacemanager
