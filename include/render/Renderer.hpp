0164dbac5e2375c15f6a084bd3d7a2bcce1c6945
jamieboy1337 <jamieboy1337@gmail.com>
2020-11-14 11:07:31 -0800

    created renderer header

---
+++ include/render/Renderer.hpp
@@ -0,0 +1,14 @@
+#ifndef RENDERER_H_
+#define RENDERER_H_
+namespace monkeysworld {
+namespace render {
+
+/**
+ *  Renderer dispatched to element hierarchy to draw components.
+ */ 
+class Renderer {
+
+};
+
+}  
+}
