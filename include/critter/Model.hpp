0f68c7e5e8160f70e6ba205cdda162f424ca4a32
jamieboy1337 <jamieboy1337@gmail.com>
2020-11-15 21:45:11 -0800

    added model header

---
+++ include/critter/Model.hpp
@@ -0,0 +1,16 @@
+#ifndef MODEL_H_
+#define MODEL_H_
+
+#include <critter/GameObject.hpp>
+
+namespace monkeysworld {
+namespace critter {
+
+class Model : public GameObject {
+
+};
+
+} // namespace critter
+} // namespace monkeysworld
+
+#endif  // MODEL_H_
