ccc6066e5eba58766f245de44b041abff6205a2c
jamieboy1337 <jamieboy1337@gmail.com>
2020-11-15 21:44:59 -0800

    added collidable interface

---
+++ include/critter/Collidable.hpp
@@ -0,0 +1,19 @@
+#ifndef COLLISION_OBJECT_H_
+#define COLLISION_OBJECT_H_
+
+#include <critter/GameObject.hpp>
+
+namespace monkeysworld {
+namespace critter {
+
+/**
+ *  Interface implemented by all objects which can be collided with.
+ */ 
+class Collidable {
+
+};
+
+}
+}
+
+#endif
