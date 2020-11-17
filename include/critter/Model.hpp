b988f2965b128fcdf2fc5d88f02054e216654df8
jamieboy1337 <jamieboy1337@gmail.com>
2020-11-16 18:56:25 -0800

    added model overrides

--- include/critter/Model.hpp
+++ include/critter/Model.hpp
@@ -2,12 +2,30 @@
 #define MODEL_H_
 
 #include <critter/GameObject.hpp>
+#include <critter/Context.hpp>
 
 namespace monkeysworld {
 namespace critter {
 
+/**
+ *  A type of GameObject which contains visible geometry.
+ */ 
 class Model : public GameObject {
+ public:
 
+  /**
+   *  Creates a new Model
+   */ 
+  Model(Context* ctx);
+  /**
+   *  Binds VAO associated with vertices.
+   */ 
+  void PrepareAttributes() override;
+
+  /**
+   *  Draws the object onto the screen.
+   */ 
+  void RenderMaterial() override;
 };
 
 } // namespace critter
