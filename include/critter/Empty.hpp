26b80a6dad5b82fe43739a49d5da05799e3d219e
jamieboy1337 <jamieboy1337@gmail.com>
2020-12-17 12:58:20 -0800

    created empty as a hierarchy helper

---
+++ include/critter/Empty.hpp
@@ -0,0 +1,28 @@
+#ifndef EMPTY_H_
+#define EMPTY_H_
+
+#include <critter/GameObject.hpp>
+#include <critter/Context.hpp>
+
+namespace monkeysworld {
+namespace critter {
+
+/**
+ *  Empties are associated with 
+ */ 
+class Empty : public GameObject {
+ public:
+  
+  // create a new empty
+  Empty(Context* ctx) : GameObject(ctx) {}
+
+  // nop
+  void PrepareAttributes() override {}
+  void RenderMaterial() override {}
+};
+
+
+}
+}
+
+#endif
