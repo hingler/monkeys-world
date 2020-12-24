e6e5da49b266e48463d4e75a2144a0545730abc6
jamieboy1337 <jamieboy1337@gmail.com>
2020-12-24 12:02:25 -0800

    added render ctx

---
+++ include/engine/RenderContext.hpp
@@ -0,0 +1,18 @@
+#ifndef RENDER_CONTEXT_H_
+#define RENDER_CONTEXT_H_
+
+namespace monkeysworld {
+namespace engine {
+
+/**
+ *  A context which contains information regarding the scene which is used
+ *  by components while being rendered.
+ */ 
+class RenderContext {
+
+}
+
+}
+}
+
+#endif
