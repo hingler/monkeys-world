d4242a5ff8e27b0b9c4ca87ed4b06846f80ba9bd
jamieboy1337 <jamieboy1337@gmail.com>
2021-01-02 00:29:08 -0800

    impl for render context

--- include/engine/RenderContext.hpp
+++ include/engine/RenderContext.hpp
@@ -1,6 +1,13 @@
 #ifndef RENDER_CONTEXT_H_
 #define RENDER_CONTEXT_H_
 
+#include <shader/light/LightTypes.hpp>
+#include <critter/GameCamera.hpp>
+
+#include <memory>
+#include <unordered_map>
+#include <vector>
+
 namespace monkeysworld {
 namespace engine {
 
@@ -12,6 +19,25 @@ class RenderContext {
   // lights
   // current active camera
   // i think thats all we need for now
+
+ public:
+  /**
+   *  Returns a reference to the game camera.
+   */ 
+  critter::camera_info GetActiveCamera() const;
+
+  /**
+   *  Returns a list of infos for all spotlights. Client can handle however it wants to.
+   */ 
+  const std::vector<shader::light::spotlight_info>& GetSpotlights() const;
+
+  // setters
+  void SetActiveCamera(std::shared_ptr<critter::GameCamera> cam);
+  void SetSpotlights(const std::vector<shader::light::spotlight_info>& spotlights);
+ private:
+  std::vector<shader::light::spotlight_info> spotlights_;
+  critter::camera_info cam_info_;
+
 };
 
 }
---
+++ src/engine/RenderContext.cpp
@@ -0,0 +1,28 @@
+#include <engine/RenderContext.hpp>
+
+namespace monkeysworld {
+namespace engine {
+
+using critter::camera_info;
+using shader::light::spotlight_info;
+using critter::GameCamera;
+
+camera_info RenderContext::GetActiveCamera() const {
+  return cam_info_;
+}
+
+const std::vector<spotlight_info>& RenderContext::GetSpotlights() const {
+  return spotlights_;
+}
+
+void RenderContext::SetActiveCamera(std::shared_ptr<GameCamera> cam) {
+  cam_info_ = cam->GetCameraInfo();
+}
+
+void RenderContext::SetSpotlights(const std::vector<spotlight_info>& spotlights) {
+  // this copy could incur a cost? idk
+  spotlights_ = spotlights;
+}
+
+}
+}
