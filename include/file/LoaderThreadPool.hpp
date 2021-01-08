6e77758f06092d092c68ac0eb3f1583e489decae
jamieboy1337 <jamieboy1337@gmail.com>
2021-01-08 13:54:13 -0800

    created simple thread pool for loader

---
+++ include/file/LoaderThreadPool.hpp
@@ -0,0 +1,65 @@
+#ifndef LOADER_THREAD_POOL_H_
+#define LOADER_THREAD_POOL_H_
+
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <vector>
+
+namespace monkeysworld {
+namespace file {
+
+/**
+ *  A simple thread pool implementation used by our file loaders.
+ */ 
+class LoaderThreadPool {
+ public:
+  LoaderThreadPool(int num_threads);
+
+  // method which accepts a task (which is intrinsically tied to a future) and computes it
+  // create a promise
+  // pass the promise to this
+  // return the future contained in that promise
+
+  /**
+   *  Adds a new task to the thread pool.
+   *  @param task - a lambda which will be run by this thread pool.
+   */ 
+  void AddTaskToQueue(std::function<void()> func);
+
+  ~LoaderThreadPool();
+  LoaderThreadPool& operator=(const LoaderThreadPool& other) = delete;
+  LoaderThreadPool(const LoaderThreadPool& other) = delete;
+
+  LoaderThreadPool& operator=(LoaderThreadPool&& other);
+  LoaderThreadPool(LoaderThreadPool&& other);
+ private:
+  // threads are always running
+  // threads should also rely on a kill signal telling them to wind down
+  // when a thread completes a task, it will check the task queue for something to do
+  // if there's nothing there, it should wait on a cond var
+
+  // meanwhile:
+  // when we add a task to the queue, notify one thread.
+  // that thread will pluck the task off the queue!
+
+  /**
+   *  Function used by threads.
+   *  @param flag - flag associated with this thread's execution
+   */ 
+  void threadfunc_(std::atomic_flag* flag);
+
+  int num_threads_;
+  std::thread* threads_;                            // array of threads
+  std::atomic_flag* flags_;                         // flags for each thread
+  std::queue<std::function<void()>> task_queue_;    // queue of tasks to run
+  std::mutex queue_lock_;                           // lock for accessing task queue
+  std::condition_variable task_condvar_;            // condition var for threads
+};
+
+}
+}
+
+#endif
---
+++ src/file/LoaderThreadPool.cpp
@@ -0,0 +1,63 @@
+#include <file/LoaderThreadPool.hpp>
+
+namespace monkeysworld {
+namespace file {
+
+LoaderThreadPool::LoaderThreadPool(int num_threads) {
+  threads_ = new std::thread[num_threads];
+  flags_ = new std::atomic_flag[num_threads];
+  num_threads_ = num_threads;
+  for (int i = 0; i < num_threads_; i++) {
+    threads_[i] = std::thread(&threadfunc_, this, flags_ + i);
+  }
+}
+
+void LoaderThreadPool::AddTaskToQueue(std::function<void()> func) {
+  std::unique_lock(queue_lock_);
+  task_queue_.push(func);
+  task_condvar_.notify_all();
+}
+
+void LoaderThreadPool::threadfunc_(std::atomic_flag* flag) {
+  std::function<void()> task;
+  while (flag->test_and_set()) {
+    {
+      std::unique_lock queue_lock(queue_lock_);
+      while (task_queue_.empty()) {
+        if (!flag->test_and_set()) {
+          return;
+        }
+
+        task_condvar_.wait(queue_lock);
+      }
+
+      task = task_queue_.front();
+      task_queue_.pop();
+    }
+
+    task();
+  }
+}
+
+LoaderThreadPool::~LoaderThreadPool() {
+  if (num_threads_ > 0) {
+    for (int i = 0; i < num_threads_; i++) {
+      // notify all threads to wind down
+      flags_[i].clear();
+    }
+
+    // start up the threads
+    task_condvar_.notify_all();
+    for (int i = 0; i < num_threads_; i++) {
+      // join as they wind down
+      threads_[i].join();
+    }
+
+    // delete heap allocated threads and flags
+    delete[] threads_;
+    delete[] flags_;
+  }
+}
+
+}
+}
