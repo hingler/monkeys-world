bd68849130b6c218af831f4f5f4643e8218236d6
jamieboy1337 <jamieboy1337@gmail.com>
2021-01-12 00:36:03 -0800

    started skybox class

---
+++ include/critter/SkyBox.hpp
@@ -0,0 +1,19 @@
+#ifndef SKYBOX_H_
+#define SKYBOX_H_
+
+#include <critter/GameObject.hpp>
+
+namespace monkeysworld {
+namespace critter {
+
+/**
+ *  A skybox containing a particular sky texture.
+ */  
+class SkyBox : public GameObject {
+
+};
+
+}
+}
+
+#endif
