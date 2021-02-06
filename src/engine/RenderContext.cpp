bdd941662b063ee36b840c74fc111982a9555ed2
jamieboy1337 <jamieboy1337@gmail.com>
2021-02-06 01:02:33 -0800

    create default camera if one cannot be found

--- src/engine/RenderContext.cpp
+++ src/engine/RenderContext.cpp
@@ -1,4 +1,5 @@
 #include <engine/RenderContext.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 namespace monkeysworld {
 namespace engine {
@@ -20,7 +21,18 @@ RenderPass RenderContext::GetRenderPass() const {
 }
 
 void RenderContext::SetActiveCamera(std::shared_ptr<Camera> cam) {
-  cam_info_ = cam->GetCameraInfo();
+  if (cam) {
+    cam_info_ = cam->GetCameraInfo();
+  } else {
+    // create a default camera
+    cam_info_.position = glm::vec3(0, 0, 0);
+    // need screen res here -- how could we pass it in?
+    // create a stub camera which is passed in by the engine only if the camera cannot be found
+    glm::mat4 persp = glm::perspective(45.0f, 1.85f, 0.01f, 1000.0f);
+    cam_info_.view_matrix = glm::mat4(1.0);
+    cam_info_.persp_matrix = persp;
+    cam_info_.vp_matrix = persp;
+  }
 }
 
 // store ref to vector instead of storing the vector itself? we set it up in engine and don't touch it
