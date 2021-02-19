1a7c288e36bee061cb3b5315779319be5f6d0451
jamieboy1337 <jamieboy1337@gmail.com>
2021-02-18 16:10:00 -0800

    fixed paths lol

--- src/shader/materials/FillMaterial.cpp
+++ src/shader/materials/FillMaterial.cpp
@@ -10,16 +10,16 @@ namespace materials {
 using engine::Context;
 FillMaterial::FillMaterial() {
   fill_prog_ = ShaderProgramBuilder()
-                 .WithVertexShader("resources/glsl/fill-mat.vert")
-                 .WithFragmentShader("resources/glsl/fill-mat.frag")
+                 .WithVertexShader("resources/glsl/fill-mat/fill-mat.vert")
+                 .WithFragmentShader("resources/glsl/fill-mat/fill-mat.frag")
                  .Build();
   color_cache_ = glm::vec4(glm::vec3(0.0), 1.0);
 }
 FillMaterial::FillMaterial(Context* context) {
   auto loader = context->GetCachedFileLoader();
   fill_prog_ = ShaderProgramBuilder(loader)
-                 .WithVertexShader("resources/glsl/fill-mat.vert")
-                 .WithFragmentShader("resources/glsl/fill-mat.frag")
+                 .WithVertexShader("resources/glsl/fill-mat/fill-mat.vert")
+                 .WithFragmentShader("resources/glsl/fill-mat/fill-mat.frag")
                  .Build();
   color_cache_ = glm::vec4(glm::vec3(0.0), 1.0);
 }
