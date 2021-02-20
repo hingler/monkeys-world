27ff37c13e992c06b69342dd20593a252f6da3b6
jamieboy1337 <jamieboy1337@gmail.com>
2021-02-20 02:19:58 -0800

    update uigroup to avoid drawing offscreen elements

--- src/critter/ui/UIGroup.cpp
+++ src/critter/ui/UIGroup.cpp
@@ -76,62 +76,78 @@ void UIGroup::DrawUI(glm::vec2 min, glm::vec2 max, shader::Canvas canvas) {
   glGetIntegerv(GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS, &max_textures_);
   UIGroupPacket p;
   std::shared_ptr<UIObject> child;
+  glm::vec2 group_dims = GetDimensions();
   GLuint textures[4];
   float opacities[4];
-  for (int i = 0; i < children_.size(); i += TEXTURES_PER_CALL) {
-    mesh_.Clear();
-    // todo: implement some system so that items which fall outside the bounds
-    // of the group are not drawn.
-    for (int j = 0; j < TEXTURES_PER_CALL && (i + j) < children_.size(); j++) {
-      child = children_[i + j];
-      
-      textures[j] = child->GetFramebufferColor();
-      opacities[j] = child->GetOpacity();
-      p.index = static_cast<float>(j);
-
-      auto pos = child->GetPosition();
-      auto dims = GetDimensions();
-
-      auto dims_child = child->GetDimensions();
-
-      // top left
-      p.pos.x = (pos.x / dims.x) * 2 - 1;
-      p.pos.y = 1 - (pos.y / dims.y) * 2;
-
-
-      p.texcoord = glm::vec2(0, 1);
-      mesh_.AddVertex(p);
-
-      // bottom left
-      p.pos.y -= (dims_child.y / dims.y) * 2;
-      p.texcoord = glm::vec2(0, 0);
-      mesh_.AddVertex(p);
-
-      // bottom right
-      p.pos.x += (dims_child.x / dims.x) * 2;
-      p.texcoord = glm::vec2(1, 0);
-      mesh_.AddVertex(p);
-
-      // top right
-      p.pos.y += (dims_child.y / dims.y) * 2;
-      p.texcoord = glm::vec2(1, 1);
-      mesh_.AddVertex(p);
-
-      mesh_.AddPolygon(4 * j, 4 * j + 1, 4 * j + 2);
-      mesh_.AddPolygon(4 * j, 4 * j + 2, 4 * j + 3);
-      // position / fb size
-      // (position + dimensions) / fb size
-      // tex coords always 0 - 1
-      // index defined by the order we get to it in
+  int current_child = 0;
+  int index = 0;
+  mesh_.Clear();
+  while (current_child < children_.size()) {
+    // add shit to our mesh as we go
+    // once we hit 4: draw it
+    // if we're done, and index > 0: draw it
+    child = children_[current_child];
+    glm::vec2 min_coord = child->GetPosition();
+    glm::vec2 max_coord = min_coord + child->GetDimensions();
+    if (min_coord.x > group_dims.x || min_coord.y > group_dims.y || max_coord.x < 0 || max_coord.y < 0) {
+      current_child++;
+      continue;
     }
 
+    textures[index] = child->GetFramebufferColor();
+    opacities[index] = child->GetOpacity();
+    p.index = static_cast<float>(index);
+
+    auto pos = child->GetPosition();
+    auto dims = GetDimensions();
+
+    auto dims_child = child->GetDimensions();
+
+    // top left
+    p.pos.x = (pos.x / dims.x) * 2 - 1;
+    p.pos.y = 1 - (pos.y / dims.y) * 2;
+
+
+    p.texcoord = glm::vec2(0, 1);
+    mesh_.AddVertex(p);
+
+    // bottom left
+    p.pos.y -= (dims_child.y / dims.y) * 2;
+    p.texcoord = glm::vec2(0, 0);
+    mesh_.AddVertex(p);
+
+    // bottom right
+    p.pos.x += (dims_child.x / dims.x) * 2;
+    p.texcoord = glm::vec2(1, 0);
+    mesh_.AddVertex(p);
+
+    // top right
+    p.pos.y += (dims_child.y / dims.y) * 2;
+    p.texcoord = glm::vec2(1, 1);
+    mesh_.AddVertex(p);
+
+    mesh_.AddPolygon(4 * index, 4 * index + 1, 4 * index + 2);
+    mesh_.AddPolygon(4 * index, 4 * index + 2, 4 * index + 3);
+
+    current_child++;
+    index++;
+
+    if (index == 4) {
+      mat_.SetTextures(textures);
+      mat_.SetOpacity(opacities);
+      mesh_.PointToVertexAttribs();
+      mat_.UseMaterial();
+      glDrawElements(GL_TRIANGLES, static_cast<int>(mesh_.GetIndexCount()), GL_UNSIGNED_INT, (void*)0);
+
+      index = 0;
+    }
+  }
+
+  if (index > 0) {
     mat_.SetTextures(textures);
     mat_.SetOpacity(opacities);
-
-    // point attribs
     mesh_.PointToVertexAttribs();
     mat_.UseMaterial();
-
     glDrawElements(GL_TRIANGLES, static_cast<int>(mesh_.GetIndexCount()), GL_UNSIGNED_INT, (void*)0);
   }
 }
